fbc30d734242ac8eeff703273106855b897a064e
Anonymous <email@gmail.com>
2024-10-30 10:58:17 +0100

    Add main script to run material simulations & modified input file
    + main.py now parses arguments for which simulation method is to
      be used (EMT or LennardJones) and it also properly creates and
      reads the input TOME file. These are used in md.py which is
      called by this script.
    + md.py now uses the arguments from argparse and it also uses the
      data in the input file for all the simulation parameters.
    + formatted all the relevant files with isort and black
    + updated requirements.txt

--- inttest.py
+++ inttest.py
@@ -1,31 +1,36 @@
 """script for integration testing the code"""
 
-import toml
-import sys, unittest, os
+import os
 import os.path
+import sys
+import unittest
+
+import toml
 
 from create_input_file import create_input_file
 
+
 class InputFileTests(unittest.TestCase):
     def check_input_file_types(self, input_file_name):
-        with open(input_file_name, 'r') as file:
+        with open(input_file_name, "r") as file:
             input_data = toml.load(file)
 
         # TODO lägg till fler assertIsInstance
-        self.assertIsInstance(input_data['material'], str)
+        self.assertIsInstance(input_data["material"], str)
 
     def test_create_input_test_file(self):
-        input_file_name = 'test_file.toml'
+        input_file_name = "test_file.toml"
         create_input_file(input_file_name)
-        if (os.path.exists(input_file_name)):
+        if os.path.exists(input_file_name):
             self.assertTrue(True)
         self.check_input_file_types(input_file_name)
         os.remove(input_file_name)
 
     def test_input_file_exists(self):
-        input_file_name = 'input_data.toml'
+        input_file_name = "input_data.toml"
         self.assertTrue(os.path.exists(input_file_name))
         self.check_input_file_types(input_file_name)
+
 
 if __name__ == "__main__":
     tests = [unittest.TestLoader().loadTestsFromTestCase(InputFileTests)]
