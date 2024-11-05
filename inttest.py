25845444740eb2ac996b67d5fdd87495ce97fc5a
Fabian <fabfi969@student.liu.se>
2024-11-05 11:33:33 +0100

    Restructured main.py to use function call. Added test to check if code gives errors when run.

--- inttest.py
+++ inttest.py
@@ -7,11 +7,14 @@ import unittest
 
 import toml
 
+from unittest.mock import patch
 from create_input_file import create_input_file
+from main import run_program
 
 
 class InputFileTests(unittest.TestCase):
     def check_input_file_types(self, input_file_name):
+        print("Code check 1")
         with open(input_file_name, "r") as file:
             input_data = toml.load(file)
 
@@ -19,6 +22,7 @@ class InputFileTests(unittest.TestCase):
         self.assertIsInstance(input_data["atoms"]["materials"][0], str)
 
     def test_create_input_test_file(self):
+        print("Code check 2")
         input_file_name = "test_file.toml"
         create_input_file(input_file_name)
         if os.path.exists(input_file_name):
@@ -27,13 +31,24 @@ class InputFileTests(unittest.TestCase):
         os.remove(input_file_name)
 
     def test_input_file_exists(self):
+        print("Code check 3")
         input_file_name = "input_data.toml"
         self.assertTrue(os.path.exists(input_file_name))
         self.check_input_file_types(input_file_name)
 
+     
+
+class SystemTest(unittest.TestCase):
+    def test_program_running(self):
+        print("Code check 4")
+        with unittest.mock.patch('sys.argv', ['-a', "EMT"]):
+            run_program()
+            self.assertTrue(True)
+
 
 if __name__ == "__main__":
-    tests = [unittest.TestLoader().loadTestsFromTestCase(InputFileTests)]
-    testsuite = unittest.TestSuite(tests)
+    input_tests = unittest.TestLoader().loadTestsFromTestCase(InputFileTests)
+    system_tests = unittest.TestLoader().loadTestsFromTestCase(SystemTest)
+    testsuite = unittest.TestSuite([input_tests, system_tests])
     result = unittest.TextTestRunner(verbosity=0).run(testsuite)
     sys.exit(not result.wasSuccessful())
--- main.py
+++ main.py
@@ -7,18 +7,22 @@ import toml
 from create_input_file import create_input_file
 from md import run_md
 
-parser = argparse.ArgumentParser(description="Parses simulation parameters.")
+def run_program():
+    parser = argparse.ArgumentParser(description="Parses simulation parameters.")
 
-parser.add_argument(
-    "simulation_method",
-    type=str,
-    choices=["EMT", "LennardJones"],
-    help="Simulation method",
-)
-args = parser.parse_args()
+    parser.add_argument(
+        "simulation_method",
+        type=str,
+        choices=["EMT", "LennardJones"],
+        help="Simulation method",
+    )
+    args = parser.parse_args()
 
-input_file_name = "input_data.toml"
-create_input_file(input_file_name)
-input_data = toml.load(input_file_name)
+    input_file_name = "input_data.toml"
+    create_input_file(input_file_name)
+    input_data = toml.load(input_file_name)
 
-run_md(args, input_data)
+    run_md(args, input_data)
+
+if __name__ == "__main__":
+    run_program()
