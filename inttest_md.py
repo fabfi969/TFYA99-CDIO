e7c62a7b6bd7010d8cadccaf8d6c18e84bc42e91
pauka833 <email@gmail.com>
2024-11-15 12:31:27 +0100

    Add the option to simulate different atom structures
    + the program can now simulate FCC, BCC, diamond and simle cubic
      structures
    + added corresponding tests
    + added lattice constant in the input file
    + added a file with function that creates the atom object used in
      md.py

--- inttest_md.py
+++ inttest_md.py
@@ -10,44 +10,32 @@ import toml
 from unittest.mock import patch
 from create_input_file import create_input_file
 from main import run_program
+from md import run_md
 
-
-class InputFileTests(unittest.TestCase):
-    def check_input_file_types(self, input_file_name):
-        with open(input_file_name, "r") as file:
-            input_data = toml.load(file)
-
-        self.assertIsInstance(input_data["atoms"]["directions"], list)
-        self.assertIsInstance(input_data["atoms"]["materials"][0], str)
-        self.assertIsInstance(input_data["atoms"]["x_size"], (int, float))
-        self.assertIsInstance(input_data["atoms"]["y_size"], (int, float))
-        self.assertIsInstance(input_data["atoms"]["z_size"], (int, float))
-        self.assertIsInstance(input_data["atoms"]["pbc"], bool)
-
-        self.assertIsInstance(input_data["lennard_jones"]["atomic_number"][0], int)
-        self.assertIsInstance(input_data["lennard_jones"]["epsilon"], (int, float))
-        self.assertIsInstance(input_data["lennard_jones"]["sigma"], (int, float))
-        self.assertIsInstance(input_data["lennard_jones"]["r_cut"], (int, float))
-        self.assertIsInstance(input_data["lennard_jones"]["modified"], bool)
-        
-        self.assertIsInstance(input_data["temperature_K"], (int, float))
-        self.assertIsInstance(input_data["time_step"], (int, float))
-        self.assertIsInstance(input_data["trajectory_file_name"], str)
-        self.assertIsInstance(input_data["trajectory_interval"], (int, float))
-
+class inputFileTests(unittest.TestCase):
     def test_create_input_test_file(self):
         input_file_name = "test_file.toml"
         create_input_file(input_file_name)
         if os.path.exists(input_file_name):
             self.assertTrue(True)
-        self.check_input_file_types(input_file_name)
         os.remove(input_file_name)
 
     def test_input_file_exists(self):
         input_file_name = "input_data.toml"
         self.assertTrue(os.path.exists(input_file_name))
-        self.check_input_file_types(input_file_name)
-     
+
+    def test_structure_parameter(self):
+        input_file_name = "input_data.toml"
+        create_input_file(input_file_name)
+        input_data = toml.load(input_file_name)
+
+        structure_list = ['BodyCenteredCubic', 'Diamond', 'FaceCenteredCubic', 'SimpleCubic']
+        for structure in structure_list:
+            for simulation_method in ['EMT', 'LennardJones']:
+                input_data['atoms']['structure'] = structure
+                run_md(simulation_method, input_data)
+                self.assertTrue(True)
+
 
 class SystemTest(unittest.TestCase):
     def test_program_running_EMT(self):
@@ -77,12 +65,9 @@ class SystemTest(unittest.TestCase):
             run_program()
             self.assertTrue(True)
 
-
-
-
 if __name__ == "__main__":
-    input_tests = unittest.TestLoader().loadTestsFromTestCase(InputFileTests)
     system_tests = unittest.TestLoader().loadTestsFromTestCase(SystemTest)
-    testsuite = unittest.TestSuite([input_tests, system_tests])
+    input_file_tests = unittest.TestLoader().loadTestsFromTestCase(inputFileTests)
+    testsuite = unittest.TestSuite([system_tests, input_file_tests])
     result = unittest.TextTestRunner(verbosity=0).run(testsuite)
     sys.exit(not result.wasSuccessful())
