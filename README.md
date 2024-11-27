07a9721b9e16e36aaabee5f859323826e0db5404
pauka833 <email@gmail.com>
2024-11-27 18:05:42 +0100

    Add slurm printouts to md.py and conversion from slurm to CSV

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Material simulation software description.
 ## How to run program
 
 1. Open terminal in program folder.
-2. Type: 
+2. Type:
 
         python3 main.py
     This will run the code with EMT, without visualisation and in constant energy mode.
@@ -51,11 +51,22 @@ Material simulation software description.
         -lattice_constant float
     This overrides the lattice constant set in the input file.
 
+        -slurm
+    This will tell the program to print in a way that the outputs in the terminal can be turned into a CSV-file.
+
 ## Run on supercomputer
 1. Run 'sbatch super_comp_script.q'
-2. Simulation method, ensable mode, etc. can be modified by appending the corresponding option to 'python3 main.py' in the same way as when program is run in terminal, see [How to run program](#how-to-run-program). 
+2. Simulation method, ensable mode, etc. can be modified by appending the corresponding option to 'python3 main.py' in the same way as when program is run in terminal, see [How to run program](#how-to-run-program).
 3. Output file will have name on format slurm-xxxxxxx.out
 4. If code does not run properly, check if requirments are corectly installed, see Installation.
+
+## Results from Supercomputer
+1. Push up the slurm-file to data/slurms
+2. On a local computer run:
+    ```bash
+    python3 slurm_to_csv.py
+3. Use pandas to read the CSV into Python
+
 
 ## Edit README
 1. open README
--- md.py
+++ md.py
@@ -1,5 +1,6 @@
 '''Demonstrates molecular dynamics with constant energy. Is called by main'''
 
+import sys
 from asap3 import EMT, LennardJones, Trajectory # This line gives the terminal warnings.
 from ase import units
 from ase.lattice.cubic import BodyCenteredCubic, BodyCenteredCubicFactory, Bravais, Diamond
@@ -14,13 +15,12 @@ from ase.io import read
 from ase.md import Andersen
 import numpy as np
 from create_input_file import create_input_file
-from create_atoms_md import invalid_materials_EMT, create_atoms
+from create_atoms_md import invalid_materials_EMT_error, create_atoms
 import toml
 from calculate_properties import calcenergy, calctemperature, calcpressure
 from save_data import writetofile
 from random import random
 from alloy import Interface
-
 
 def TwoBlocks(mat1, structure1, a1, mat2, structure2, a2, size, alloy_ratio = 0, alloy = "N"):
     #Generate an two layers of atoms pressed up against each other
@@ -46,9 +46,20 @@ def random_alloys(mat1,structure1,a1,mat2,atomic_percent,size):
     return(bulk1)
     #view(bulk1)
 
-
 def run_md(args, input_data):
     '''runs the molecular dynamics simulation'''
+
+    # deletes asap3 warnings in terminal
+    if args.slurm:
+        nr_delete_terminal_lines = 6
+    else:
+        nr_delete_terminal_lines = 3
+    for _ in range(nr_delete_terminal_lines):
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
+        sys.stdout.flush()
+    print("____Starting new simulation____:")
+
 
     #command line override of lattice constant
     if args.lattice_constant != -1:
@@ -64,12 +75,7 @@ def run_md(args, input_data):
 
     # Describe the interatomic interactions with the Effective Medium Theory
     if args.simulation_method == 'EMT':
-        invalid_materials_status = invalid_materials_EMT(atoms.symbols)
-        if invalid_materials_status[0]:
-            print('ERROR:\n    The defined elements cannot be simulated using EMT.\n    EMT only supports \
-the metals Al, Cu, Ag, Au, Ni, Pd and Pt.')
-            print(f'    The defined elements are {invalid_materials_status[1]}.')
-            quit()
+        invalid_materials_EMT_error(atoms.symbols)
         atoms.calc = EMT()
 
     elif args.simulation_method == 'LennardJones':
@@ -100,16 +106,18 @@ the metals Al, Cu, Ag, Au, Ni, Pd and Pt.')
         dyn = Andersen(atoms, input_data['time_step'], input_data['temperature_K'], 1 * units.fs)
 
     traj = Trajectory(input_data['trajectory_file_name'], 'w', atoms)
-    # TODO check what next line does
     dyn.attach(traj.write, interval=input_data['trajectory_interval'])
 
     def printenergy(a=atoms):  # store a reference to atoms in the definition.
         '''Function to print the potential, kinetic and total energy.'''
         epot, ekin, etot = calcenergy(a)
-        print(
-            'Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-            'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), etot)
-        )
+        if args.slurm:
+            print(f"{epot},{ekin},{ekin / (1.5 * units.kB)},{etot}")
+        else:
+            print(
+                'Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
+                'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), etot)
+            )
 
     f = open('output_data.txt', 'w') # Open the target file. Overwrite existing file.
     epot_list, ekin_list, etot_list, temperature_list, pressure_list = ([] for i in range(5))
@@ -131,7 +139,9 @@ the metals Al, Cu, Ag, Au, Ni, Pd and Pt.')
     savedata()
     printenergy()
     dyn.run(input_data['run_time'])
-    writetofile()
+    if not args.slurm:
+        writetofile()
+    print("-----End of simulation.-----")
 
 if __name__ == '__main__':
     input_file_name = 'input_data.toml'
