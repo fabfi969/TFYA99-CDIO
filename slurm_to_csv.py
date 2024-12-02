4398e05733cf2f10f8824ee821fa378ca5c54f1a
pauka833 <email@gmail.com>
2024-12-02 16:55:05 +0100

    slurm_to_csv only writes the equilibrium values, all in the same file

--- slurm_to_csv.py
+++ slurm_to_csv.py
@@ -4,39 +4,39 @@ import os
 import csv
 
 def slurm_to_csv():
-    csv_categories = ['Epot,Ekin,T,Etot']
+    '''this parses the slurm files in data/slurms and creates CSV-files out of them located in data/slurm_results'''
+
+
+    # These are the categories written to the CSV
+    # TODO: ADD CATEGORIES THAT DIFFERENTIATE THE RUNS ON THE NEXT LINE, AKA thickness or material combination or something
+    csv_categories = 'Epot,Ekin,T,Etot'
+
 
     root_path = (os.getcwd()).split('TFYA99-CDIO')[0] + 'TFYA99-CDIO'
-
     RES_PATH = f'{root_path}/data/slurm_results'
+
     if not os.path.exists(RES_PATH):
         os.makedirs(RES_PATH)
 
     for slurm_name in os.listdir(f'{root_path}/data/slurms'):
         print(slurm_name)
 
-        if not os.path.exists(f'{RES_PATH}/{slurm_name}'):
-            os.makedirs(f'{RES_PATH}/{slurm_name}')
+        with open(f'{RES_PATH}/{slurm_name}.csv', mode='w', newline='', encoding='UTF-8') as file:
+            writer = csv.writer(file)
 
-        with open(f'{root_path}/data/slurms/{slurm_name}', 'r', encoding='UTF-8') as f:
-            lines = f.readlines()
+            writer.writerow((csv_categories).split(','))
 
-        SIMUL_NR = 1
-        csv_file_lines = csv_categories[:]
-        for line in lines:
+            with open(f'{root_path}/data/slurms/{slurm_name}', 'r', encoding='UTF-8') as f:
+                lines = f.readlines()
 
-            if (line[0]).isdigit():
-                csv_file_lines.append(line[:-1])
-
-            elif line[0] == '-':
-
-                with open(f'{RES_PATH}/{slurm_name}/simulation_{SIMUL_NR}.csv',
-                            mode='w', newline='', encoding='UTF-8') as file:
-                    writer = csv.writer(file)
-                    for line in csv_file_lines:
-                        writer.writerow(line.split(','))
-                SIMUL_NR += 1
-                csv_file_lines = csv_categories[:]
+            csv_file_lines = []
+            for line in lines:
+                if (line[0]).isdigit():
+                    csv_file_lines.append(line[:-1])
+                elif line[0] == '-':
+                    # only write the values when the system has stabilized
+                    writer.writerow((csv_file_lines[-1]).split(','))
+                    csv_file_lines = []
 
 if __name__ == '__main__':
     slurm_to_csv()
