9f548eb4f4d12a50b56b29574b1f8da99f457974
Joakim Berggren <joabe712@student.liu.se>
2024-12-07 22:28:43 +0100

    Added function to plot output data from csv file.

---
+++ interface_energy_plot.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore", message = "Unable to import Axes3D")
+import matplotlib.pyplot as plt # This would give a warning without above line.
+import ast
+import pandas
+import os
+
+def plot_interface_energy_vs_atomic_concentration(file_name):
+    '''Plot interface energy vs atomic concentration.
+    Note: At the moment this plots other things.'''
+    base_path = os.getcwd()  
+    relative_path = os.path.join("data", "slurm_results", file_name) 
+    file_path = os.path.join(base_path, relative_path)
+    data = pandas.read_csv(file_path)
+    x = data['Ekin'].tolist() 
+    y = data['Epot'].tolist()
+    plt.scatter(x, y)
+    plt.title('Potential energy vs kinetic energy.')
+    plt.xlabel('Kinetic energy')
+    plt.ylabel('Potential energy')
+    plt.show()
+    
+
+#file_name = input("Type the CSV file name:")
+file_name = 'slurm-4056000.out.csv'
+plot_interface_energy_vs_atomic_concentration(file_name)
