a2dc3dbc896d913a0b800b3c5259baede172ee00
pauka833 <pauka833@student.liu.se>
2024-12-13 11:13:55 +0100

    Update pres.py

--- pres.py
+++ pres.py
@@ -6,6 +6,6 @@ os.system("python3 create_input_file.py")
 os.system("python3 main.py -view_atoms -visualisation on -slurm")
 # theory: bulk modulus = 103.6 GPa (PH), 2.95 Ev (p50, v8, solid state physics, Kittel)
 
-# os.system("python3 main.py -view_atoms -simulation_method LennardJones -visualisation on")
+os.system("python3 main.py -view_atoms -simulation_method LennardJones -visualisation on")
 
-# os.system("python3 main.py -view_atoms -simulation_method Interface")
+os.system("python3 main.py -view_atoms -simulation_method Interface")
