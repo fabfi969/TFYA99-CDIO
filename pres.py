f27b9cfe7d2133714669eb985e805614c1db3c78
pauka833 <email@gmail.com>
2024-12-13 10:54:06 +0100

    add example file for running the program

--- pres.py
+++ pres.py
@@ -1,14 +1,9 @@
 import os
 
-os.system("python3 create_input_file_1.py")
-# os.system("python3 main.py -view_atoms -visualisation on")
+os.system("python3 create_input_file.py")
+os.system("python3 main.py -view_atoms -visualisation on -slurm")
+# theory: bulk modulus = 103.6 GPa (PH), 2.95 Ev (p50, v8, solid state physics, Kittel)
 
-os.system("python3 main.py -simulation_method LennardJones")
+# os.system("python3 main.py -view_atoms -simulation_method LennardJones -visualisation on")
 
-
-
-# os.system("python3 create_input_file_2.py")
-# os.system("python3 main.py -view_atoms")
-
-
-
+# os.system("python3 main.py -view_atoms -simulation_method Interface")
