602755bf259ae2726e1ddec27b8ac71239884731
Fabian Finnström <x_fabfi@sigma.nsc.liu.se>
2024-12-13 09:38:24 +0100

    Forgot to commit md.py

--- md.py
+++ md.py
@@ -228,14 +228,15 @@ def run_md(args, input_data):
     # Now run the dynamics
     dyn.attach(printenergy, interval=input_data['trajectory_interval'])
     dyn.attach(savedata, interval=input_data['trajectory_interval'])
-    dyn.attach(volumes_and_energies, interval=input_data['trajectory_interval'])
+    # dyn.attach(volumes_and_energies, interval=input_data['trajectory_interval'])
     savedata()
     printenergy()
-    volumes_and_energies()
+    # volumes_and_energies()
     dyn.run(input_data['run_time'])
 
     cohesive_energy = calccohesiveenergy(epot_list, input_data['atoms']['materials'], atoms.calc)
-    bulk_modulus = calcbulkmodulus(volumes, energies)
+    #bulk_modulus = calcbulkmodulus(volumes, energies)
+    bulk_modulus = 1
     if is_equilibrium():
         if args.slurm:
 
